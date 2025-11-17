98985c4660d7b0f300a8adbcb60da42d2fcf6a77
dinesh-veer <dveer123@hotmail.com>
2025-11-17 22:09:55 +0530

    Adding Unnamed class example.

---
+++ src/main/java/com/dinesh/unnamed/classes/UnnamedClass_1.java
@@ -0,0 +1,16 @@
+//this package declaration will give compile time error
+//package com.dinesh.unnamed.patternAndVariables;
+
+// No 'public class ...' wrapper needed!
+// No 'static' or 'String[] args' needed!
+void main(){
+    System.out.println("Welcome to java 21 !!!!");
+}
+
+
+//to compile using command line use below
+//javac -source 21 --enable-preview UnnamedClass_1.java
+//or
+//javac --release 21 --enable-preview UnnamedClass_1.java
+//to run the program use below command
+//java --enable-preview UnnamedClass_1
