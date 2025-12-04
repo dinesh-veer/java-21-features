4298795c413bb7f9987e198f11ff308df898d80e
dinesh-veer <dveer123@hotmail.com>
2025-12-04 12:43:10 +0530

    Adding tricky example.

---
+++ src/main/java/com/dinesh/trickyExamples/StringTemplateExample.java
@@ -0,0 +1,33 @@
+package com.dinesh.trickyExamples;
+
+import static java.lang.StringTemplate.RAW;
+
+public class StringTemplateExample {
+
+    public static void main(String[] args) {
+
+        String name = "java 21";
+
+        //Difference between below temple and string
+        StringTemplate stringTemplate = StringTemplate.of(STR."Welcome to \{name}");
+        String string = STR."Welcome \{name}";
+
+        System.out.println(string);//evaluated String produced by the processor STR
+        System.out.println(stringTemplate);//template object containing fragments + values (not a String yet).
+
+
+        //Difference between below
+        String user = "Robert'); DROP TABLE Students;--";
+        String withoutTemplate = STR."SELECT * FROM users WHERE name = '\{user}'";
+        StringTemplate st = RAW."SELECT * FROM users WHERE name = '\{user}'";
+
+        System.out.println(withoutTemplate);
+        System.out.println(st.toString());
+//        RAW creates a StringTemplate object; it doesn’t concatenate or send to DB.
+//        You must explicitly validate and escape values before turning it into a String.
+//        So this prevents SQL injection until you manually build and execute the query.
+
+
+
+    }
+}
